40b9f2134a2f128bcd4ebaf50514ee541b402660
shaheermalik2275-source <shaheermalik2275@gmail.com>
2025-11-19 10:54:11 +0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,47 @@
+// java/com/example/myapp/MainActivity.java
+package com.example.L1SBSCS0324;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSettings, btnProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSettings = findViewById(R.id.btnSettings);
+        btnProfile = findViewById(R.id.btnProfile);
+
+        // Show Settings fragment first (you can change to Profile if you want)
+        replaceFragment(new SettingsFragment());
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new SettingsFragment());
+            }
+        });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new ProfileFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
